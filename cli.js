0b9268fd874418860abd6bbed1c892f521073c4e
Stephan Besser <stephan.besser@gmail.com>
2021-06-13 14:48:09 +0200

    wip: cli
    Signed-off-by: Stephan Besser <stephan.besser@gmail.com>

--- cli.js
+++ cli.js
@@ -7,18 +7,30 @@ const fs = require('fs');
 program
     .option('-f, --input-filepath <inFilepath>', 'The input file path to the openAIR file')
     .option('-o, --output-filepath <outFilepath>', 'The output filename of the generated geojson file')
+    .option(
+        '-V, --validate-geometry',
+        'If set to true, validates geometries. Defaults to true.',
+        true
+    )
+    .option(
+        '-F, --fix-geometry',
+        'If set to true, tries to fix geometries. Defaults to false.',
+        false
+    )
     .parse(process.argv);
 
 (async () => {
-    const parser = new Parser();
-    const result = await parser.parse(program.inputFilepath);
 
-    const { success, errors, geojson } = result;
-    if (success) {
+    // console.log(program);
+    console.log(program.validateGeometry);
+    console.log(program.fixGeometry);
+
+    const parser = new Parser({ validateGeometry: program.validateGeometry, fixGeometry: program.fixGeometry });
+    try {
+        await parser.parse(program.inputFilepath);
+        const geojson = parser.toGeojson();
         await fs.writeFileSync(program.outputFilepath, Buffer.from(JSON.stringify(geojson, null, 2), 'utf-8'));
-    } else {
-        for (const error of errors) {
-            console.log(error);
-        }
+    } catch (e) {
+        console.log(e.message);
     }
 })();
