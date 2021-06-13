4945f3d1b5d5341ec1c13415b559b0231e590a72
Stephan Besser <stephan.besser@gmail.com>
2021-06-13 19:12:27 +0200

    cli
    Signed-off-by: Stephan Besser <stephan.besser@gmail.com>

--- cli.js
+++ cli.js
@@ -7,28 +7,19 @@ const fs = require('fs');
 program
     .option('-f, --input-filepath <inFilepath>', 'The input file path to the openAIR file')
     .option('-o, --output-filepath <outFilepath>', 'The output filename of the generated geojson file')
-    .option(
-        '-V, --validate-geometry',
-        'If set to true, validates geometries. Defaults to true.',
-        true
-    )
-    .option(
-        '-F, --fix-geometry',
-        'If set to true, tries to fix geometries. Defaults to false.',
-        false
-    )
+    .option('-V, --validate', 'If set to true, validates geometries. Defaults to true.')
+    .option('-F, --fix-geometry', 'If set to true, tries to fix geometries. Defaults to false.')
     .parse(process.argv);
 
 (async () => {
+    const validateGeometry = program.validateGeometry || true;
+    const fixGeometry = program.fixGeometry || false;
 
-    // console.log(program);
-    console.log(program.validateGeometry);
-    console.log(program.fixGeometry);
-
-    const parser = new Parser({ validateGeometry: program.validateGeometry, fixGeometry: program.fixGeometry });
+    const parser = new Parser({ validateGeometry, fixGeometry });
     try {
         await parser.parse(program.inputFilepath);
         const geojson = parser.toGeojson();
+        console.log(`Successfully parsed ${geojson.features.length} airspaces` );
         await fs.writeFileSync(program.outputFilepath, Buffer.from(JSON.stringify(geojson, null, 2), 'utf-8'));
     } catch (e) {
         console.log(e.message);
