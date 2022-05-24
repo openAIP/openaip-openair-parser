8dd0cd24d65887fe4c91424626843c912b6dfe82
Stephan Besser <stephan.besser@gmail.com>
2022-05-24 16:47:52 +0200

    Fixes openaip/openaip#11

--- src/tokens/skipped-token.js
+++ src/tokens/skipped-token.js
@@ -15,11 +15,11 @@ class SkippedToken extends CommentToken {
         checkTypes.assert.string(line);
 
         // line contains a skipped token
-        return /^(AT|TO|TC|SP|SB|DY).*$/.test(line);
+        return /^(AT|TO|TC|SP|SB).*$/.test(line);
     }
 
     tokenize(line, lineNumber) {
-        const token = new CommentToken({ tokenTypes: this.tokenTypes });
+        const token = new SkippedToken({ tokenTypes: this.tokenTypes });
 
         checkTypes.assert.string(line);
         checkTypes.assert.integer(lineNumber);
