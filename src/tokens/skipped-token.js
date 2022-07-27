dccf8fde5192c0d0423d732f78284b374d2091de
Stephan Besser <stephan.besser@gmail.com>
2022-07-27 10:49:35 +0200

    ignore AF tokens

--- src/tokens/skipped-token.js
+++ src/tokens/skipped-token.js
@@ -15,7 +15,7 @@ class SkippedToken extends CommentToken {
         checkTypes.assert.string(line);
 
         // line contains a skipped token
-        return /^(AT|TO|TC|SP|SB|V Z=\d).*$/.test(line);
+        return /^(AT|TO|TC|SP|SB|V Z=\d|AF).*$/.test(line);
     }
 
     tokenize(line, lineNumber) {
