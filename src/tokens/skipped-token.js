e6440f1333ea83d0e6a65ee488dac7bbb354772c
Stephan Besser <stephan.besser@gmail.com>
2022-07-27 11:03:16 +0200

    skip token AG

--- src/tokens/skipped-token.js
+++ src/tokens/skipped-token.js
@@ -15,7 +15,7 @@ class SkippedToken extends CommentToken {
         checkTypes.assert.string(line);
 
         // line contains a skipped token
-        return /^(AT|TO|TC|SP|SB|V Z=\d|AF).*$/.test(line);
+        return /^(AT|TO|TC|SP|SB|V Z=\d|AF|AG).*$/.test(line);
     }
 
     tokenize(line, lineNumber) {
