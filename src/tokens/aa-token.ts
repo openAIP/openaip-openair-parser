8e11ecbfaef3c0ba6b693e4541d9a8e80c798fc2
Hennie Brink <Wackymax@users.noreply.github.com>
2025-08-21 09:19:53 +0200

    Fix broken ESM imports

--- src/tokens/aa-token.ts
+++ src/tokens/aa-token.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { ParserError } from '../parser-error.js';
 import { validateSchema } from '../validate-schema.js';
-import { AbstractLineToken, type IToken } from './abstract-line-token';
+import { AbstractLineToken, type IToken } from './abstract-line-token.js';
 import { TokenTypeEnum, type TokenType } from './token-type.enum.js';
 
 export const BY_NOTAM_ACTIVATION = 'BY_NOTAM';
